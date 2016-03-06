e78a8deecdcc3445bd1cb5603cd115298b50a408
Curtis Lowe <lowecurt4@gmail.com>
2016-03-05 22:33:59 -0700

    Lesson 8 Individual Assignment

---
+++ Leaving Planet Earth/src/citbyui/cit260/LeavingPlanetEarth/view/FoodMenuView.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.LeavingPlanetEarth.view;
+
+/**
+ *
+ * @author Curtis
+ */
+public class FoodMenuView extends View {
+    
+    public FoodMenuView() {
+        super("\n"
+                    + "\n------------------------"
+                    + "\n| Food Menu                |"
+                    + "\n------------------------"
+                    + "\nF - Fruit                       "
+                    + "\nV - Vegetables              "
+                    + "\nW - Water                    "
+                    + "\nR - Return to Game         ");  
+    }
+    
+    @Override
+    public boolean doAction(String value) {
+    
+    value = value.toUpperCase();   // convert all to upper case
+    char choice = value.charAt (0);// convert choice to upper case
+    
+    switch (choice) {
+        case 'F':
+            this.displayFruit();
+            break;
+        case 'V':
+            this.displayVegetable();
+            break;
+        case 'W':
+            this.displayWater();
+            break;
+        case 'R': // return to game
+            return true;
+        default:
+            System.out.println("\n*** Invalide selection *** Try again");
+        }
+        return false;
+    }
+    
+    private void displayFruit() {
+        System.out.println("\n You have 5 fruits");
+    }
+    private void displayVegetable() {
+        System.out.println("\n You have 5 vegetables");
+    }
+    private void displayWater() {
+        System.out.println("\n You have 2 gallons of water");
+    }
+}
