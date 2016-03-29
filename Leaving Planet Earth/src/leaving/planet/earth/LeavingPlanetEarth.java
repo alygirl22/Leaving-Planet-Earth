78c1b46e3627219bd0c7a3dc1fd6a3213a1d8b6f
alygirl22 <aly_cheers17@yahoo.com>
2016-03-28 22:12:02 -0600

    Update LeavingPlanetEarth.java

--- Leaving Planet Earth/src/leaving/planet/earth/LeavingPlanetEarth.java
+++ Leaving Planet Earth/src/leaving/planet/earth/LeavingPlanetEarth.java
@@ -18,15 +18,60 @@ public class LeavingPlanetEarth {
 
     private static Game currentGame = null;
     private static Player player = null;
-    /**
-     * @param args the command line arguments
-     */
+    
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+
+    private static PrintWriter logFile = null;
+    
     public static void main(String[] args) {
         
-        //create StartProgramViewOrig and display the start program view
+        try {
+            
+            // open character stream files for end user input and output
+            LeavingPlanetEarth.inFile =
+                    new BufferedReader (new InputStreamReader (System.in));
+            
+            LeavingPlanetEarth.outFile = new PrintWriter(System.out, true);
+            
+            // open log file
+            String filePath = "log.txt";
+            LeavingPlanetEarth.logFile = new PrintWriter(filePath);
+            
+            //create StartProgramViewOrig and display the start program view
         StartProgramView startProgramView = new StartProgramView();
         startProgramView.display();
+        return;
+            
+        } catch (Throwable e) {
+            
+                System.out.println("Exception: " + e.toString() +
+                                          "\nCause: " + e.getCause() +
+                                          "\nMessage: " + e.getMessage());
+                
+                e.printStackTrace();;
+        }
+        
+        finally {
+            
+            try {
+               if (LeavingPlanetEarth.inFile != null)
+                   LeavingPlanetEarth.inFile.close();
+               
+               if (LeavingPlanetEarth.outFile != null)
+                   LeavingPlanetEarth.outFile.close();
+               
+               if (LeavingPlanetEarth.logFile != null)
+                   LeavingPlanetEarth.logFile.close();
+        } catch (IOException ex) {
+            System.out.println("Error closing files");
+            return;
+        }
+        
+        
     }
+        
+}
      
     public static Game getCurrentGame() {
         return currentGame;
@@ -41,4 +86,28 @@ public class LeavingPlanetEarth {
     public static void setPlayer(Player player){
         LeavingPlanetEarth.player = player;
     }
+        public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        LeavingPlanetEarth.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        LeavingPlanetEarth.inFile = inFile;
+    }
+
+    public static PrintWriter getLogFile() {
+        return logFile;
+    }
+
+    public static void setLogFile(PrintWriter logFile) {
+        LeavingPlanetEarth.logFile = logFile;
+    }
+    
 }
